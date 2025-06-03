71bf88b4e48a07192d4514d0c9b9d31f570fd679
Amirsam.A <samistheman888@gmail.com>
2025-06-03 10:47:23 -0400

    deleting the testing done in repotest.java

--- repotest.java
+++ repotest.java
@@ -3,11 +3,6 @@ import arc.*;
 public class repotest{
 	public static void main(String[] args) {
 		Console con = new Console();
-		con.println("Hello word");
-		con.println("TGIF");
-		con.println("CPT Time!");
-		con.println("push to origin");
-		con.println("push to ");
-		con.println("push to ");
+		
 	}
 }
