64f73e22f9e85f77ddadafc9f5355a90c3dfab34
Amirsam.A <samistheman888@gmail.com>
2025-06-05 10:45:21 -0400

    created menu screen and will add function to the directed inputs
    ex. 1 = play 2 = leaderboard etc.

--- ToolsCPT.java
+++ ToolsCPT.java
@@ -1,9 +1,15 @@
 import arc.*;
 
 public class ToolsCPT{
-	public static void main(String[] args) {
-		Console con = new Console();
-		
-		
+	
+	public static void menuprint (Console con){
+		con.println("press 1 to play");
+		con.println("press 2 for leaderboard");
+		con.println("press 3 to add a theme");
+		con.println("press 4 to quit");
 	}
+	// public static double 
+	
+	
 }
+
--- cptamirsam1.java
+++ cptamirsam1.java
@@ -3,6 +3,12 @@ import arc.*;
 public class CPTAmirsam1{
 	public static void main(String[] args) {
 		Console con = new Console();
+		
+		
+		ToolsCPT.menuprint(con);
+		
+		
+		
 
 	}
 }
