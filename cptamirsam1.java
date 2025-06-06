31a9a24ad18c25bcfadf878ee6ac056198d432c8
Amirsam.A <samistheman888@gmail.com>
2025-06-06 11:02:52 -0400

    created all the screens for the game & added functioning to them
    - added screens
    - added secret screen
    - added functions
    - need to fix error

--- cptamirsam1.java
+++ cptamirsam1.java
@@ -3,12 +3,21 @@ import arc.*;
 public class CPTAmirsam1{
 	public static void main(String[] args) {
 		Console con = new Console();
-		
-		
-		ToolsCPT.menuprint(con);
-		
-		
-		
-
+				
+		while(true) {
+			int choice = ToolsCPT.menuprint(con);
+			
+			if (choice == 1){
+				ToolsCPT.playprint(con);
+			} else if (choice == 2) {
+				ToolsCPT.leaderboardprint(con);
+			} else if (choice == 3) {
+				ToolsCPT.themeprint(con);
+			} else if (choice == 4 ) {
+				System.exit(0);
+			} else {
+				con.println("Ivalid choice. Try again.");
+			}
+		}	
 	}
 }
