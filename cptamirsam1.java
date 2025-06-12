ba858eea106c83f8db980c944d2ecd2a6402c056
Amirsam.A <116230681+Amirsam-A@users.noreply.github.com>
2025-06-12 03:56:37 -0400

    -

--- cptamirsam1.java
+++ cptamirsam1.java
@@ -1,5 +1,4 @@
 import arc.*;
-aimport arc.*;
 
 public class cptamirsam1 {
     public static void main(String[] args) {
@@ -33,46 +32,6 @@ public class cptamirsam1 {
             } else if (strChoice.equals("S")) {
                 con.println("Secret Joke: Why do programmers hate nature? It has too many bugs!");
                 con.readLine(); // pause
-            } else if (strChoice.equals("Q")) {
-                con.println("Thanks for playing!");
-                break;
-            }
-        }
-    }
-}
-
-public class cptamirsam1 {
-    public static void main(String[] args) {
-        Console con = new Console();
-        ThemeManager themeManager = new ThemeManager();
-        LeaderboardManager leaderboard = new LeaderboardManager();
-        Game game = new Game();
-        String strChoice;
-
-        while (true) {
-            con.clear();
-            con.println("=== Hangman ===");
-            con.println("(P)lay Game");
-            con.println("(L)eaderboard");
-            con.println("(A)dd Theme");
-            con.println("(H)elp");
-            con.println("(Q)uit");
-
-            strChoice = con.getChar() + "";
-            strChoice = strChoice.toUpperCase();
-
-            if (strChoice.equals("P")) {
-                game.play(con, themeManager, leaderboard);
-            } else if (strChoice.equals("L")) {
-                leaderboard.displayLeaderboard(con);
-            } else if (strChoice.equals("A")) {
-                themeManager.addTheme(con);
-            } else if (strChoice.equals("H")) {
-                con.println("Help: Choose 'Play Game' to start, then guess the full word.");
-                con.readLine(); 
-            } else if (strChoice.equals("S")) {
-                con.println("Secret Joke: What did the plate say to the other plate? Dinner is on me");
-                con.readLine(); 
             } else if (strChoice.equals("Q")) {
                 con.println("Thanks for playing!");
                 break;
